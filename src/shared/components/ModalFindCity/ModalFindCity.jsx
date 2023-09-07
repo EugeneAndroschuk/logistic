a6a990747bc1aa5c616d2a439b099cd062001cee
EugeneAndroschuk <111381321+EugeneAndroschuk@users.noreply.github.com>
2023-09-07 15:58:10 +0300

    add useEffect for token

--- src/shared/components/ModalFindCity/ModalFindCity.jsx
+++ src/shared/components/ModalFindCity/ModalFindCity.jsx
@@ -1,7 +1,7 @@
 import PropTypes from "prop-types";
 import axios from "axios";
-import { useEffect, useState} from "react";
-import { useSelector} from "react-redux";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 import useDebounce from "../../../utils/useDebounce";
 import { getUserToken } from "../../../redux/auth/authSelectors";
 import CloseIcon from "@mui/icons-material/Close";
@@ -12,6 +12,7 @@ const ModalFindCity = ({ toggleModal, onFindCity }) => {
   const [search, setSearch] = useState("");
   const [value, setValue] = useState("");
   const [cities, setCities] = useState([]);
+  const [isAmadeusTokenExpired, setIsAmadeusTokenExpired] = useState(true);
   const token = useSelector(getUserToken);
   const tokenApi = import.meta.env.VITE_AMADEUS_API_TOKEN;
   // const controller = new AbortController();
@@ -19,7 +20,27 @@ const ModalFindCity = ({ toggleModal, onFindCity }) => {
   const LOCALHOST_URL = "http://localhost:3000";
   const DEPLOY_URL = "https://logistics-db.onrender.com";
 
-  const debouncedSearch = useDebounce((value)=>setSearch(value), 1100);
+  const debouncedSearch = useDebounce((value) => setSearch(value), 1100);
+
+  useEffect(() => {
+    if (!isAmadeusTokenExpired) return;
+
+    console.log("sending request=========")
+
+    axios(
+      {
+        method: "POST",
+        url: "https://test.api.amadeus.com/v1/security/oauth2/token",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        data: {
+          grant_type: "client_credentials",
+          client_id: import.meta.env.VITE_AMADEUS_API_KEY,
+          client_secret: import.meta.env.VITE_AMADEUS_API_SECRET,
+        },
+      }
+    ).then(res=>console.log(res));
+
+  },[isAmadeusTokenExpired]);
 
   useEffect(() => {
     if (search === "") return;
@@ -27,16 +48,8 @@ const ModalFindCity = ({ toggleModal, onFindCity }) => {
     const controller = new AbortController();
     const signal = controller.signal;
 
-    const lardiTransFetch = async () => {
+    const findCity = async () => {
       try {
-        // const response = await axios.get(
-        //   `${DEPLOY_URL}/api/drives/findcity?city=${search}`,
-        //   {
-        //     headers: { Authorization: `Bearer ${token}` },
-        //     signal,
-        //   }
-        // );
-
         const response = await axios.get(
           `https://test.api.amadeus.com/v1//reference-data/locations/cities?keyword=${search}&max=10`,
           {
@@ -52,18 +65,35 @@ const ModalFindCity = ({ toggleModal, onFindCity }) => {
         setCities([...citiesArray]);
       } catch (e) {
         if (!signal?.aborted) {
-          console.error(e);
+          const errorCode = e.response.data.errors[0].code;
+          if (errorCode === 38192) setIsAmadeusTokenExpired(true);
         }
       }
     };
 
-    lardiTransFetch();
+    // const lardiTransFetch = async () => {
+    //   try {
+    //     // const response = await axios.get(
+    //     //   `${DEPLOY_URL}/api/drives/findcity?city=${search}`,
+    //     //   {
+    //     //     headers: { Authorization: `Bearer ${token}` },
+    //     //     signal,
+    //     //   }
+    //     // );
+    //   } catch (e) {
+    //     if (!signal?.aborted) {
+    //       const errorCode = e.response.data.errors[0].code;
+    //       if (errorCode === 38192) setIsAmadeusTokenExpired(true);
+    //     }
+    //   }
+    // };
+
+    findCity();
 
     return () => {
       controller.abort();
     };
-
-  },[search, token, tokenApi]);
+  }, [search, tokenApi]);
 
   const onHandleInput = (e) => {
     setValue(e.target.value);
