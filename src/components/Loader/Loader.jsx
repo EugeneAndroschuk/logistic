ae93e4a584d39ffb4331675a1acb15987d3d885d
EugeneAndroschuk <111381321+EugeneAndroschuk@users.noreply.github.com>
2023-09-07 14:29:58 +0300

    fix Loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,10 +1,8 @@
 
 import { ThreeDots } from "react-loader-spinner";
-import css from "./Loader.module.css";
 
 const Loader = () => {
   return (
-    <div className={css.loader}>
       <ThreeDots
         height="80"
         width="80"
@@ -15,7 +13,6 @@ const Loader = () => {
         wrapperClassName=""
         visible={true}
       />
-    </div>
   );
 };
 
