18d59fd29fba6b6d64ad3d8339b660cab1391e17
EugeneAndroschuk <111381321+EugeneAndroschuk@users.noreply.github.com>
2023-09-07 14:25:39 +0300

    add Loader to authPage

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -2,9 +2,11 @@
 import { Outlet } from "react-router-dom";
 import { useSearchParams } from "react-router-dom";
 import { useEffect, useMemo } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { googleAuth } from "../../redux/auth/authThunks";
 import { setToken } from "../../redux/auth/authSlice";
+import Loader from "src/components/Loader/Loader";
+import { getUserIsRefreshing } from "../../redux/auth/authSelectors";
 import { AuthPageWrap } from "./AuthPage.styled";
 
 const AuthPage = () => {
@@ -15,8 +17,8 @@ const AuthPage = () => {
     [searchParams]
   );
   const { token } = params;
-
   const dispatch = useDispatch();
+  const isUserRefreshing = useSelector(getUserIsRefreshing);
 
   useEffect(() => {
     
@@ -27,9 +29,7 @@ const AuthPage = () => {
   }, [dispatch, token]);
 
   return (
-    <AuthPageWrap>
-        <Outlet />
-    </AuthPageWrap>
+    <AuthPageWrap>{isUserRefreshing ? <Loader /> : <Outlet />}</AuthPageWrap>
   );
 };
 
