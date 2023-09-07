b464b7da070f559dafff07b5d1add00f07bbc0b2
EugeneAndroschuk <111381321+EugeneAndroschuk@users.noreply.github.com>
2023-09-07 14:38:46 +0300

    fix styles

--- src/components/Loader/Loader.styled.js
+++ src/components/Loader/Loader.styled.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const LoaderWrap = styled.div`
   height: 100%;
-  width: 100%;
+  width: 100vw;
   display: flex;
   justify-content: centr;
   align-items: center;
